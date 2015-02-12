87e1d3d05ae621a315ffb6407650ce457de52f10
Daniel Griesser <daniel.griesser@bytepoets.com>
2015-02-12 11:22:10 +0100

    removed wrong license

--- Publiss/PUBAppDelegate.m
+++ Publiss/PUBAppDelegate.m
@@ -24,15 +24,7 @@
     PUBConfig.sharedConfig.appToken = @"1d3ae766-0206-4eb7-90e1-f2e2917a4635";
     PUBConfig.sharedConfig.appSecret = @"9fc4153103bd73cbe36c88738dc9e8bb";
     
-    [Publiss.staticInstance setupWithLicenseKey:@"T/b4cf+SB2t6JcWU/Izbkzkt8j70K2Zc9S5ubSdyI6cuJm1Q0tBv8eQz7DpN"
-     "J0/RAblrGUgI2T7mHc3vpqPHEAjd8XG9J3naWNgbrhQvaMhUmv7RBVI4tvAf"
-     "/S2PJDvUyrx63cz/wNIEnVFxQTGfZvqFNaKMFLQMEUqGbRVzRZHE553GATeX"
-     "77BnVcMOoQSPgEV6jrSLkvuYEa0XNDG6ZE/xNkaRViwW70Mmsa71YY9gKjz0"
-     "T6fbD/hjcZtzNBc701VGYBSmA6AkymVMShF3lPgEXyTdbRq6g38tNDn+Tls6"
-     "6yAWCMOw1cxnCHtX05E/yEbHC8JlsLiE7R5T04PeMwVLvjWdBJ0HTmUQfInj"
-     "hIz5VPt36/rzM3bbS0bIZ4T6R2APaWugHU6YqHGDP3/PYdaSa+kJ8zudC0qj"
-     "wB8b3JEbO8KSFdcArmZFLe2/A40Di7jQjuf9R/i4fLMF22/KQhFx6qvrntUQ"
-     "eKjpcLFhULk+m5OJ5Ap6yAJ6gy0DaCEouw13MS9Tl+GfP+6kzg5GMw=="];
+    [Publiss.staticInstance setupWithLicenseKey:nil];
     
     // Uncomment this line to use the language tag feature.
     //PUBConfig.sharedConfig.preferredLanguage = [[NSLocale currentLocale] objectForKey:NSLocaleLanguageCode];
